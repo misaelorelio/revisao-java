9c0db95698870c6206d9cdf3bb596f7dfb7943d5
misaelorelio <misaelorelio@hotmail.com>
2021-04-22 19:20:24 -0300

    sobrecarga de metodos

--- aulaUdemy/src/aulaUdemy/armazenamento/Funcionario.java
+++ aulaUdemy/src/aulaUdemy/armazenamento/Funcionario.java
@@ -2,33 +2,90 @@ package aulaUdemy.armazenamento;
 
 public class Funcionario {
 
-	String nome;
-	String departamento;
-	double salario;
-	String dataEntrada;
-	String rg;
-	boolean estaNaEmpresa;
-	double ganhoAnual;
+	private String nome;
+	private String departamento;
+	private double salario;
+	private String dataEntrada;
+	private String rg;
+	private boolean estaNaEmpresa;
+	private double ganhoAnual;
 
 	void calculaSalario(double valor) {
 		this.salario = valor - (valor * 0.06);
 	}
 
-	void calculaSalario(double valor, double horaExtra) {
+	public void calculaSalario(double valor, double horaExtra) {
 		double valorHoraExtra = (valor / 30) / 8 * horaExtra;
 		this.salario = valor - (valor * 0.06) + valorHoraExtra;
 	}
 
-	void bonifica(double valor) {
+	public void bonifica(double valor) {
 		salario += valor;
 	}
 
-	void demite() {
+	public void demite() {
 		estaNaEmpresa = false;
 	}
 
 	double ganhoAnual() {
 		return salario * 12;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(String dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public boolean isEstaNaEmpresa() {
+		return estaNaEmpresa;
+	}
+
+	public void setEstaNaEmpresa(boolean estaNaEmpresa) {
+		this.estaNaEmpresa = estaNaEmpresa;
+	}
+
+	public double getGanhoAnual() {
+		return ganhoAnual;
+	}
+
+	public void setGanhoAnual(double ganhoAnual) {
+		this.ganhoAnual = ganhoAnual;
+	}
+	
 
 }
--- aulaUdemy/src/aulaUdemy/armazenamento/Programa2.java
+++ aulaUdemy/src/aulaUdemy/armazenamento/Programa2.java
@@ -5,9 +5,12 @@ public class Programa2 {
 	public static void main(String[] args) {
 
 		Funcionario f1 = new Funcionario();
+		f1.setRg("212121212");
+		f1.setDepartamento("Estágiario");
 		f1.calculaSalario(1000, 2);
 		
-		System.out.println(f1.salario);
+		System.out.println(f1.getSalario());
+		System.out.println(f1.getRg());
 	}
 
 }
