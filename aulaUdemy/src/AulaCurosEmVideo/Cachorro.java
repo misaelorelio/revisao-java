147a2b0e0f9e9cf929415f343e1b0c2cbc64f93d
misaelorelio <misaelorelio@hotmail.com>
2021-04-28 20:01:40 -0300

    polimorfismo animais

--- aulaUdemy/src/AulaCurosEmVideo/Cachorro.java
+++ aulaUdemy/src/AulaCurosEmVideo/Cachorro.java
@@ -6,4 +6,57 @@ public class Cachorro extends Lobo {
 	public void emitirSom() {
 		System.out.println("au au au au");
 	}
+
+	public void reagir(String frase) {
+
+		if (frase.equals("toma comida") || frase.equals("Olá"))
+			System.out.println("Abanar e latir");
+
+		else {
+			System.out.println("Rosnar");
+		}
+
+	}
+
+	public void reagir(int hora, int min) {
+		if (hora < 12) {
+			System.out.println("Abanar");
+
+		} else if (hora >= 18) {
+			System.out.println("Ignorar");
+		} else {
+			System.out.println("Abanar e latir");
+		}
+
+	}
+
+	public void reagir(boolean dono) {
+
+		if(dono) {
+			System.out.println("Abanar");
+		}
+		else {
+			System.out.println("Rosnar e latir");
+		}
+	}
+
+	public void reagir(int idade, double peso) {
+		if (idade < 5) {
+			if(peso < 10) {
+				System.out.println("Abanar");
+			}
+		} else {
+			System.out.println("Latir");
+
+		}
+		{
+			if(peso < 10) {
+				System.out.println("Rosnar");
+			}
+			else {
+				System.out.println("Ignorar");
+			}
+		}
+
+	}
 }
--- aulaUdemy/src/AulaCurosEmVideo/Programa.java
+++ aulaUdemy/src/AulaCurosEmVideo/Programa.java
@@ -4,12 +4,17 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Mamifero x = new Mamifero();
-		x.emitirSom();
+		Cachorro cachorro = new Cachorro();
+		cachorro.reagir(true);
+		cachorro.reagir(false);
+		cachorro.reagir("Olá");
+		cachorro.reagir("Vai apanhar");
+		cachorro.reagir(11, 45);
+		cachorro.reagir(21, 00);
+		cachorro.reagir(2, 12.5);
+		cachorro.reagir(17, 4.5);
 		
-		Lobo y = new Lobo();
-		y.emitirSom();
-
+		
 	}
 
 }
